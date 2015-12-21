d7650a89c7d3eca26af0a4517ef15719d2e50766
congit91 <congit.91dn@gmail.com>
2015-12-21 15:33:10 +0700

    update

--- src/servlet/AccessTokenServlet.java
+++ src/servlet/AccessTokenServlet.java
@@ -31,12 +31,14 @@ public class AccessTokenServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
@@ -45,42 +47,44 @@ public class AccessTokenServlet extends HttpServlet {
 		String code = request.getParameter("code");
 		String clientId = "7e9e9ddfeca347c5867ef1d0dd8bdbd5";
 		String redirectURI = "http://localhost:8080/InstagramApp/";
-		
-		if(code != null) {
-			String clientSecret = "e18348362e3f4248aa6c66a651f12ab7";
-			JSONObject profile = getTokenContent(clientId, clientSecret, redirectURI, code);
+
+		String clientSecret = "e18348362e3f4248aa6c66a651f12ab7";
+		JSONObject profile = getTokenContent(clientId, clientSecret, redirectURI, code);
+		if (profile.has("access_token")) {
 			String accessToken = profile.getString("access_token");
 			request.setAttribute("accessToken", accessToken);
 			request.getRequestDispatcher("page/index.jsp").forward(request, response);
 		} else {
-			String url = "https://api.instagram.com/oauth/authorize/"
-					+ "?client_id="+clientId+""
-					+ "&redirect_uri="+redirectURI+"&response_type=code&scope=basic+likes+comments+follower_list+relationships+public_content";
+			String url = "https://api.instagram.com/oauth/authorize/" + "?client_id=" + clientId + "" + "&redirect_uri="
+					+ redirectURI
+					+ "&response_type=code&scope=basic+likes+comments+follower_list+relationships+public_content";
 			response.sendRedirect(url);
 		}
+
 	}
-	
+
 	@SuppressWarnings("resource")
-	public JSONObject getTokenContent(String clientID, String clientSecret, String redirectURI, String code) throws IOException {
+	public JSONObject getTokenContent(String clientID, String clientSecret, String redirectURI, String code)
+			throws IOException {
 		HttpClient httpclient;
-	    HttpPost httppost;
-	    ArrayList<NameValuePair> postParameters;
-	    httpclient = new DefaultHttpClient();
-	    httppost = new HttpPost("https://api.instagram.com/oauth/access_token");
+		HttpPost httppost;
+		ArrayList<NameValuePair> postParameters;
+		httpclient = new DefaultHttpClient();
+		httppost = new HttpPost("https://api.instagram.com/oauth/access_token");
 
-	    postParameters = new ArrayList<NameValuePair>();
-	    postParameters.add(new BasicNameValuePair("client_id", clientID));
-	    postParameters.add(new BasicNameValuePair("client_secret", clientSecret));
-	    postParameters.add(new BasicNameValuePair("grant_type", "authorization_code"));
-	    postParameters.add(new BasicNameValuePair("redirect_uri", redirectURI));
-	    postParameters.add(new BasicNameValuePair("code", code));
+		postParameters = new ArrayList<NameValuePair>();
+		postParameters.add(new BasicNameValuePair("client_id", clientID));
+		postParameters.add(new BasicNameValuePair("client_secret", clientSecret));
+		postParameters.add(new BasicNameValuePair("grant_type", "authorization_code"));
+		postParameters.add(new BasicNameValuePair("redirect_uri", redirectURI));
+		postParameters.add(new BasicNameValuePair("code", code));
 
-	    httppost.setEntity(new UrlEncodedFormEntity(postParameters));
+		httppost.setEntity(new UrlEncodedFormEntity(postParameters));
 
-	    HttpResponse response = httpclient.execute(httppost);
-	    
-	    String json_string = EntityUtils.toString(response.getEntity());
-	    JSONObject jsonObject = new JSONObject(json_string);
-	    return jsonObject;
+		HttpResponse response = httpclient.execute(httppost);
+
+		String json_string = EntityUtils.toString(response.getEntity());
+		JSONObject jsonObject = new JSONObject(json_string);
+		return jsonObject;
 	}
 }
